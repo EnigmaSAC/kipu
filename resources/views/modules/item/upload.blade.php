9948bc69332c24a73a5a3e50b544b7cec932434d
Enigma Developers Sac <93346395+EnigmaSAC@users.noreply.github.com>
2025-08-14 21:48:40 +0200

    Wrap item upload form in content slot

--- resources/views/modules/item/upload.blade.php
+++ resources/views/modules/item/upload.blade.php
@@ -1,8 +1,11 @@
 <x-layouts.modules>
     <x-slot name="title">{{ trans('modules.upload_title') }}</x-slot>
-    <x-form method="POST" action="{{ route('apps.upload') }}" enctype="multipart/form-data">
-        <x-form.input.file name="file" accept=".zip" required />
-        <x-button type="submit">{{ trans('modules.upload_install') }}</x-button>
-    </x-form>
+
+    <x-slot name="content">
+        <x-form method="POST" action="{{ route('apps.upload') }}" enctype="multipart/form-data">
+            <x-form.input.file name="file" accept=".zip" required />
+            <x-button type="submit">{{ trans('modules.upload_install') }}</x-button>
+        </x-form>
+    </x-slot>
 </x-layouts.modules>
 
